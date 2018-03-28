b6c8175195e44ea93f087540b32049157e63a7d6
rwdavies <robertwilliamdavies@gmail.com>
2018-03-28 10:30:38 -0400

    Updates to installation for mac

--- scripts/install-LDAK.sh
+++ scripts/install-LDAK.sh
@@ -4,6 +4,12 @@ set -e
 script_dir=`dirname "$0"`
 cd "${script_dir}"/../
 
-wget http://dougspeed.com/wp-content/uploads/ldak5.linux_.zip
+${WGET_OR_CURL} http://dougspeed.com/wp-content/uploads/ldak5.linux_.zip
+${WGET_OR_CURL} http://dougspeed.com/wp-content/uploads/ldak5.mac_.zip
 unzip ldak5.linux_.zip
+unzip ldak5.mac_.zip    
+
+
+
+
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,5 +4,14 @@ set -e
 script_dir=`dirname "$0"`
 cd "${script_dir}"/../
 
+os=`uname -a | cut -f1 -d" "`
+
+if [ "${os}" == "Darwin" ]
+then
+    export WGET_OR_CURL="curl -L -O"
+else
+    export WGET_OR_CURL="wget"
+fi
+
 ./scripts/install-snakemake.sh
 ./scripts/install-LDAK.sh
