a58aa08310daa9286b10e1e832009f3aa020716d
rwdavies <robertwilliamdavies@gmail.com>
2018-04-12 13:19:19 -0400

    Include / do not include rsids

--- scripts/make-test-dataset.sh
+++ scripts/make-test-dataset.sh
@@ -9,6 +9,8 @@ mkdir -p ${TEST_RESULTS}
 TEST_PREFIX=${TEST_RESULTS}/testdata
 
 ALPHA=${1:-"-0.25"}
+MAKE_RSID=${1:-"TRUE"}
 
 echo Simulating assuming alpha=${ALPHA}
-R --slave -f R/make-test-dataset.R --args ${TEST_PREFIX} ${ALPHA}
+echo Write rsid to .map file? ${MAKE_RSID}
+R --slave -f R/make-test-dataset.R --args ${TEST_PREFIX} ${ALPHA} ${MAKE_RSID}
