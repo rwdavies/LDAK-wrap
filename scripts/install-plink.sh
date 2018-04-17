6d6da69d8d9bd0eae3dcbe2b2d01e779006f5a37
rwdavies <robertwilliamdavies@gmail.com>
2018-04-17 15:02:16 -0400

    new urls for plink

--- scripts/install-plink.sh
+++ scripts/install-plink.sh
@@ -6,10 +6,10 @@ cd "${script_dir}"/../
 
 if [ "${USER_OS}" == "Darwin" ]
 then
-    ${WGET_OR_CURL} https://www.cog-genomics.org/static/bin/plink180221/plink_mac.zip
+    ${WGET_OR_CURL} http://s3.amazonaws.com/plink2-assets/plink2_mac_20180416.zip
     unzip plink_mac.zip    
 else
-    ${WGET_OR_CURL} https://www.cog-genomics.org/static/bin/plink180221/plink_linux_x86_64.zip
+    ${WGET_OR_CURL} http://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20180416.zip
     unzip plink_linux_x86_64.zip
 fi
 
