d14cc81552282a59920dba036d257a3aafa50b46
rwdavies <robertwilliamdavies@gmail.com>
2018-04-17 15:37:10 -0400

    Force over-ride unzipping of plinkinstallation of plink

--- scripts/install-plink.sh
+++ scripts/install-plink.sh
@@ -7,10 +7,10 @@ cd "${script_dir}"/../
 if [ "${USER_OS}" == "Darwin" ]
 then
     ${WGET_OR_CURL} http://s3.amazonaws.com/plink2-assets/plink2_mac_20180416.zip
-    unzip plink2_mac_20180416.zip
+    unzip -o plink2_mac_20180416.zip
 else
     ${WGET_OR_CURL} http://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20180416.zip
-    unzip plink2_linux_x86_64_20180416.zip
+    unzip -o plink2_linux_x86_64_20180416.zip
 fi
 
 
